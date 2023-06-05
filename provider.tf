9985795c9f0e73d1cbf063b62b38c9586beb06e5
joshi927 <40684940+joshi927@users.noreply.github.com>
2023-06-05 18:35:01 +1000

    Configure lock table

--- provider.tf
+++ provider.tf
@@ -1,3 +1,13 @@
+terraform {
+  backend "s3" {
+    
+  bucket = "terraform-remote-state-abc123"
+  key = "terraform-tfstate"
+  region = "us-east-1"
+  dynamodb_table = "terraform-remote-state"
+  }
+}
+
 provider "aws" {
   region = "us-east-1"
 }
